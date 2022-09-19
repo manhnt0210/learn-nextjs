aee4455ee4f67d8b740cf38cf92b55549f7a960b
manhbnt <nguyen.the.manhb@sun-asterisk.com>
2022-09-19 11:11:01 +0700

    complete post card

--- components/home/post-card.tsx
+++ components/home/post-card.tsx
@@ -1,13 +1,30 @@
-import { Card, CardContent } from '@mui/material';
+import { Post } from '@/models/index';
+import { Card, CardContent, Divider, Typography } from '@mui/material';
+import { format } from 'date-fns';
 import * as React from 'react';
 
-export interface IPostCardProps {
+export interface PostCardProps {
+  post: Post
 }
 
-export function PostCard (props: IPostCardProps) {
+export function PostCard ({ post }: PostCardProps) {
+  if (!post) return null
+  format
   return (
     <Card>
-      <CardContent>Post title</CardContent>
+      <CardContent>
+        <Typography variant="h5" fontWeight="bold">{post.title}</Typography>
+
+        <Typography variant="body1" my={2} sx={{ display: 'flex'}}>
+          {format(Number(post.publishedDate), 'dd MMM yyyy')}
+
+          <Divider orientation="vertical" flexItem sx={{ mx: 2}}/>
+
+          {post.tagList.join(', ')}
+        </Typography>
+
+        <Typography variant="body2">{post.title}</Typography>
+      </CardContent>
     </Card>
   );
 }
--- components/home/recent-post.tsx
+++ components/home/recent-post.tsx
@@ -1,10 +1,30 @@
+import { Post } from '@/models/index';
 import { Box, Typography, Link as MuiLink, Card } from '@mui/material';
 import { Container, Stack } from '@mui/system';
 import Link from 'next/link';
 import * as React from 'react';
 import { PostCard } from './post-card';
 
+
+
 export function RecentPosts () {
+  const postList: Post[] = [
+    {
+      id: 1,
+      title: 'Making a design system',
+      publishedDate: '1663558819597',
+      tagList: ['Design', 'Pattern'],
+      description: '',
+    },
+    {
+      id: 2,
+      title: 'Creating pixel perfect',
+      publishedDate: '1663558819597',
+      tagList: ['Figma', 'Icon Design'],
+      description: '',
+    },
+  ]
+
   return (
     <Box component="section" bgcolor="secondary.light" pt={2} pb={4}>
       <Container>
@@ -19,12 +39,11 @@ export function RecentPosts () {
           xs: 'column',
           md: 'row',
         }} sx={{'& > div': {width: {xs: '100%', md: '50%'}}}} spacing={3}>
-          <Box>
-            <PostCard />
-          </Box>
-          <Box>
-            <PostCard />
-          </Box>
+          {postList.map(post => (
+            <Box key={post.id}>
+              <PostCard post={post} />
+            </Box>
+          ))}
         </Stack>
       </Container>
     </Box>
